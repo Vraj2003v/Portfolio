05396ec66724d15f707f319e35561a09c71a364b
Vraj2003v <vraj.patel2003v@gmail.com>
2026-01-04 13:28:20 -0500

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/portfolio/',
+    base: '/Portfolio/',
     plugins: [react()],
     build: {
         rollupOptions: {
             output: {
                 manualChunks(id) {
                     if (id.includes('node_modules')) {
-                        // Split the swiper plugin library into a separate chunk to avoid a large chunk size on index.js
-                        if (id.includes('swiper'))
-                            return 'swiper';
-                        return;
+                        if (id.includes('swiper')) return 'swiper';
                     }
                 }
             }
@@ -22,7 +19,12 @@ export default defineConfig({
     css: {
         preprocessorOptions: {
             scss: {
-                silenceDeprecations: ["mixed-decls", "color-functions", "global-builtin", "import"],
+                silenceDeprecations: [
+                    "mixed-decls",
+                    "color-functions",
+                    "global-builtin",
+                    "import"
+                ],
             },
         },
     },
